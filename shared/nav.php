6f7bea096b70eea4c8c2534684ae143bdb82fade
VitorSparrow027 <55920025+VitorSparrow027@users.noreply.github.com>
2019-11-27 20:59:47 -0300

    fsdgvf

--- shared/nav.php
+++ shared/nav.php
@@ -36,27 +36,52 @@
         </ul>
         <div class="form-inline my-2 my-lg-0">
 
-            <button class="btn btn-outline-light my-2 my-sm-0" type="button" data-toggle="modal" data-target="#modalLogin"><i
-                    class="fas fa-user mr-1" ></i>Login</button>
+            <button class="btn btn-outline-light my-2 my-sm-0" type="button" data-toggle="modal"
+                data-target="#modalLogin"><i class="fas fa-user mr-1"></i>Login</button>
         </div>
         <!-- Modal -->
         <div class="modal fade" id="modalLogin" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
             aria-hidden="true">
-            <div class="modal-dialog" role="document">
+            <div class="modal-dialog modal-sm" role="document">
                 <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Título do modal</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        ...
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-                        <button type="button" class="btn btn-primary">Salvar mudanças</button>
-                    </div>
+                    <form name="formLogin" class="needs-validation" novalidate>
+                        <div class="modal-header">
+                            <h5 class="modal-title">Efetuar Login</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <div class="form-group">
+                                        <label for="loginEmail">Email</label>
+                                        <input name="loginEmail" type="email" class="form-control" id="loginEmail"
+                                            required>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <div class="form-group">
+                                        <label for="loginSenha">Senha</label>
+                                        <input name="loginSenha" type="password" class="form-control" id="loginSenha"
+                                            required>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-12 text-right">
+                                    <small><a href="#" class="btn btn-link">Esqueci minha senha?</a></small>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="modal-footer">
+                            <button type="submit" class="btn btn-block btn-primary">Entrar</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
